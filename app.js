f2f71ce3ef4cc6d2345029dd01ce4f798b911693
henry <89320103+henrxG@users.noreply.github.com>
2023-10-20 23:14:15 +0200

    Basis System hinzugefügt

--- app.js
+++ app.js
@@ -12,4 +12,29 @@ function getComputerChoice() {
     return Rock;
   }
 }
-console.log(getComputerChoice());
+const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection ===1 && computerSelection === 1){
+      return "Unentschieden";
+    } else if (playerSelection === 1 && computerSelection === 2){
+      return "Du hast gewonnen";
+    } else if (playerSelection === 1 && computerSelection === 3){
+      return "Du hast verloren";
+    } else if (playerSelection === 2 && computerSelection === 1){
+      return "Du hast verloren";
+    } else if (playerSelection === 2 && computerSelection === 2){
+      return "Unentschieden";
+    } else if (playerSelection === 2 && computerSelection === 3){
+      return "Du hast gewonnen";
+    } else if (playerSelection === 3 && computerSelection === 1){
+      return "Du hast gewonnen";
+    } else if (playerSelection === 3 && computerSelection === 2){
+      return "Du hast verloren";
+    } else if (playerSelection === 3 && computerSelection === 3){
+      return "Unentschieden";
+    }
+  }
+  console.log(playRound);
